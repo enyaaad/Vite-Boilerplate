cec28cdb73395170909f9a84f0abb085d4cee07e
enyad <enindima1@gmail.com>
2023-01-23 20:04:39 +0700

    [init] Vite-Boilerplate

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Layout from "./components/layout.vue";
+</script>
+
+<template>
+  <layout></layout>
+</template>
+
+<style scoped>
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/layout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<h1>everything works</h1>
+</template>
+
+<style lang="sass" scoped>
+@import "../styles/_variables.sass"
+body
+  h1
+    text-align: center
+</style>
